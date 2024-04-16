8138525c1c7205c6df788b4e2e365de093c4e3fb
phung hoang <lnphunghoang190903@gmail.com>
2024-04-16 17:12:52 +0700

    Add completed codes

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,19 @@
 export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `This is my PC. I work mostly in FullStack projects these days.
+      I've made a couple of websites and app. I also like Computer Vision and Deep Learning. Anyway regardless of the type of projects/languages, I just like programming.
+      Here is my <a href="https://github.com/phunghoang1909" target="_blank">Github</a>!`,
+    "cs-degree": `This is my CS awards. I hung it on the wall because I'm proud of it. It was a very challenging but I think it gave me a good foundation.`,
+    "sofa-table": `That's my sofa. I like to relax here and watch YouTube.`,
+    tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+     <a href="https://www.youtube.com/@javascriptmastery" target="_blank">JavaScript Mastery</a>, <a href="https://www.youtube.com/@javascriptmastery" target="_blank"></a>,`,
+    bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+    resume: `This is my desk and on it is my resume. Contact me at lnphunghoang190903@gmail.com if you have any questions!`,
+    projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+    Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
+    Kaboom.js by watching some of tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
+    library: `There are a lot of programming books on my shelves. There is even one in Vietnamese (I also speak Vietnamese btw).
+    I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
+    exit: `If you want to exit PhungHoang1909's portfolio, just close the tab.`,
+  };
